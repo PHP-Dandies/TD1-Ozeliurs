9d4168f494e4f0950e605b3195fd6d4b3ae95c42
ozeliurs <ozeliurs@gmail.com>
2021-11-25 11:12:09 +0100

    fin td1

--- index.php
+++ index.php
@@ -1,22 +1,15 @@
 <html>
 	<head>
-
+		<link rel="stylesheet" href="https://unpkg.com/chota@latest">
+		<?php
+			$a = 1
+		?>
 	</head>
 	<body>
-		<?php
-			$var1 = 6;
-			$var2 = 1.3;
-			$var3 = 'Variable 3';
-		?>
 
-		<?php
-			echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
-			echo 'C\'est pas mal non ? <br/>';
-		?>
+		<h3><?php echo date("F j, Y, g:i a"); ?></h3>
+		<h3><?php echo date('d/m/Y'); ?></h3>
 
-		<?php echo "$var1 + $var2"; ?>
-		<?php echo $var1 + $var2; ?>
-		<?php echo $var1 + $var3; ?>
 	</body>
 </html>
 
