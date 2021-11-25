896bf914890b79e286cdf3439f0859dc4d7d7023
ozeliurs <ozeliurs@gmail.com>
2021-11-25 09:11:23 +0100

    test date

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
-<?php
-	$var1 = 6;
-	$var2 = 1.3;
-	$var3 = 'Variable 3';
-?>
+<html>
+	<head>
 
-<?php
-	echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
-	echo 'C\'est pas mal non ? <br/>';
-?>
+	</head>
+	<body>
+		<?php
+			$var1 = 6;
+			$var2 = 1.3;
+			$var3 = 'Variable 3';
+		?>
 
-<?php echo "$var1 + $var2"; ?>
-<?php echo $var1 + $var2; ?>
-<?php echo $var1 + $var3; ?>
+		<?php
+			echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
+			echo 'C\'est pas mal non ? <br/>';
+		?>
+
+		<?php echo "$var1 + $var2"; ?>
+		<?php echo $var1 + $var2; ?>
+		<?php echo $var1 + $var3; ?>
+	</body>
+</html>
+
